af307b0190e6413d35a51bbd15b9968c5cffff96
ruegerj <jan.rueger@gmx.ch>
2023-08-30 22:33:03 +0200

    add bootstrapper which waits for content load

--- scripts/shortcuts.js
+++ scripts/shortcuts.js
@@ -1,1 +1,41 @@
-console.log("This is the calendar");
+const uiElements = new Map();
+
+function bootstrapElements() {
+  uiElements.set("nextBtn", loadIconBtn("ChevronRightRegular"));
+  uiElements.set("prevBtn", loadIconBtn("ChevronLeftRegular"));
+  uiElements.set("newEventBtn", loadIconBtn("CalendarEmptyRegular"));
+  uiElements.set("viewTodayBtn", loadIconBtn("CalendarDayRegular"));
+  uiElements.set("viewWorkweekBtn", loadIconBtn("CalendarWorkWeekRegular"));
+  uiElements.set("viewWeekBtn", loadIconBtn("Calendar3DayRegular"));
+  uiElements.set("viewMonthBtn", loadIconBtn("CalendarLtrRegular"));
+  uiElements.set(
+    "todayBtn",
+    document.getElementById("id__53").parentElement.parentElement.parentElement
+  );
+  uiElements.set("searchInput", document.getElementById("topSearchInput"));
+  console.log("items", uiElements);
+}
+
+function loadIconBtn(icon) {
+  return document.querySelector(`i[data-icon-name="${icon}"]`).parentElement
+    .parentElement;
+}
+
+function waitUntilLoaded(callback) {
+  const loadingScreen = document.getElementById("loadingScreen");
+  const observer = new MutationObserver((mutations, observer) => {
+    for (const mutation of mutations) {
+      if (mutation.removedNodes[0] === loadingScreen) {
+        console.log("loaded");
+        observer.disconnect();
+        callback();
+        break;
+      }
+    }
+  });
+  observer.observe(loadingScreen.parentElement, {
+    childList: true,
+  });
+}
+
+waitUntilLoaded(bootstrapElements);
