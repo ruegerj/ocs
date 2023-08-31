1af5f298442fee00d60bdfa8e55de0d9481e06cf
ruegerj <jan.rueger@gmx.ch>
2023-08-31 17:12:56 +0200

    add editor configs

--- scripts/shortcuts.js
+++ scripts/shortcuts.js
@@ -1,85 +1,85 @@
 const KEYS = {
-  arrLeft: 37,
-  arrUp: 38,
-  arrRight: 39,
-  arrDown: 40,
-  a: 65,
-  d: 68,
-  m: 77,
-  t: 84,
-  w: 87,
+    arrLeft: 37,
+    arrUp: 38,
+    arrRight: 39,
+    arrDown: 40,
+    a: 65,
+    d: 68,
+    m: 77,
+    t: 84,
+    w: 87,
 };
 
 function scrapeElements() {
-  const uiElements = new Map();
-  uiElements.set("nextBtn", loadIconBtn("ChevronRightRegular"));
-  uiElements.set("prevBtn", loadIconBtn("ChevronLeftRegular"));
-  uiElements.set("newEventBtn", loadIconBtn("CalendarEmptyRegular"));
-  uiElements.set("viewDayBtn", loadIconBtn("CalendarDayRegular"));
-  uiElements.set("viewWorkweekBtn", loadIconBtn("CalendarWorkWeekRegular"));
-  uiElements.set("viewWeekBtn", loadIconBtn("Calendar3DayRegular"));
-  uiElements.set("viewMonthBtn", loadIconBtn("CalendarLtrRegular"));
-  uiElements.set(
-    "todayBtn",
-    document.querySelector(
-      '[data-app-section="CalendarModuleSurfaceNavigationBar"]'
-    ).children[0]
-  );
-  uiElements.set("searchInput", document.getElementById("topSearchInput"));
-  return uiElements;
+    const uiElements = new Map();
+    uiElements.set('nextBtn', loadIconBtn('ChevronRightRegular'));
+    uiElements.set('prevBtn', loadIconBtn('ChevronLeftRegular'));
+    uiElements.set('newEventBtn', loadIconBtn('CalendarEmptyRegular'));
+    uiElements.set('viewDayBtn', loadIconBtn('CalendarDayRegular'));
+    uiElements.set('viewWorkweekBtn', loadIconBtn('CalendarWorkWeekRegular'));
+    uiElements.set('viewWeekBtn', loadIconBtn('Calendar3DayRegular'));
+    uiElements.set('viewMonthBtn', loadIconBtn('CalendarLtrRegular'));
+    uiElements.set(
+        'todayBtn',
+        document.querySelector(
+            '[data-app-section="CalendarModuleSurfaceNavigationBar"]',
+        ).children[0],
+    );
+    uiElements.set('searchInput', document.getElementById('topSearchInput'));
+    return uiElements;
 }
 
 function loadIconBtn(icon) {
-  return document.querySelector(`i[data-icon-name="${icon}"]`).parentElement
-    .parentElement;
+    return document.querySelector(`i[data-icon-name="${icon}"]`).parentElement
+        .parentElement;
 }
 
 function handleKeyPress(event, elements) {
-  event.preventDefault();
+    event.preventDefault();
 
-  console.log(event.keyCode);
+    console.log(event.keyCode);
 
-  if (event.keyCode == KEYS.arrRight) {
-    elements.get("nextBtn").click();
-  } else if (event.keyCode == KEYS.arrLeft) {
-    elements.get("prevBtn").click();
-  } else if (event.keyCode == KEYS.t) {
-    elements.get("todayBtn").click();
-  }
-  // TODO: Fix click events
-  //   } else if (event.keyCode == KEYS.d) {
-  //     elements.get("viewDayBtn").click();
-  //   } else if (event.keyCode == KEYS.a) {
-  //     elements.get("viewWorkweekBtn").click();
-  //   } else if (event.keyCode == KEYS.w) {
-  //     elements.get("viewWeekBtn").click();
-  //   } else if (event.keyCode == KEYS.m) {
-  //     console.log(elements.get("viewMonthBtn"));
-  //     elements.get("viewMonthBtn").click();
+    if (event.keyCode == KEYS.arrRight) {
+        elements.get('nextBtn').click();
+    } else if (event.keyCode == KEYS.arrLeft) {
+        elements.get('prevBtn').click();
+    } else if (event.keyCode == KEYS.t) {
+        elements.get('todayBtn').click();
+    }
+    // TODO: Fix click events
+    //   } else if (event.keyCode == KEYS.d) {
+    //     elements.get("viewDayBtn").click();
+    //   } else if (event.keyCode == KEYS.a) {
+    //     elements.get("viewWorkweekBtn").click();
+    //   } else if (event.keyCode == KEYS.w) {
+    //     elements.get("viewWeekBtn").click();
+    //   } else if (event.keyCode == KEYS.m) {
+    //     console.log(elements.get("viewMonthBtn"));
+    //     elements.get("viewMonthBtn").click();
 }
 
 function bootstrap() {
-  const elements = scrapeElements();
-  document.addEventListener("keyup", (event) =>
-    handleKeyPress(event, elements)
-  );
+    const elements = scrapeElements();
+    document.addEventListener('keyup', (event) =>
+        handleKeyPress(event, elements),
+    );
 }
 
 function waitUntilLoaded(callback) {
-  const loadingScreen = document.getElementById("loadingScreen");
-  const observer = new MutationObserver((mutations, observer) => {
-    for (const mutation of mutations) {
-      if (mutation.removedNodes[0] === loadingScreen) {
-        console.log("loaded");
-        observer.disconnect();
-        callback();
-        break;
-      }
-    }
-  });
-  observer.observe(loadingScreen.parentElement, {
-    childList: true,
-  });
+    const loadingScreen = document.getElementById('loadingScreen');
+    const observer = new MutationObserver((mutations, observer) => {
+        for (const mutation of mutations) {
+            if (mutation.removedNodes[0] === loadingScreen) {
+                console.log('loaded');
+                observer.disconnect();
+                callback();
+                break;
+            }
+        }
+    });
+    observer.observe(loadingScreen.parentElement, {
+        childList: true,
+    });
 }
 
 waitUntilLoaded(bootstrap);
