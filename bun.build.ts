2b6e3bb2adf938771afd3908fe27c5cf1236aaed
ruegerj <jan.rueger@gmx.ch>
2023-09-13 21:26:19 +0200

    add custom bun build / setup tailwind

---
+++ bun.build.ts
@@ -0,0 +1,51 @@
+import { copyFile } from 'node:fs/promises';
+
+(async () => {
+    const output = await Bun.build({
+        entrypoints: [
+            'src/worker.js',
+            'src/scripts/shortcuts.js',
+            'src/popup/popup.js',
+        ],
+        outdir: 'bin',
+        minify: true,
+        target: 'browser',
+        splitting: false,
+    });
+
+    if (!output.success) {
+        console.error('❌ JS compile & bundle failed:');
+        console.error(output.logs.join('\n'));
+        return process.exit(1);
+    }
+
+    console.log('🤖JS compiled & bundled');
+
+    const tailwindArgs = [
+        '-i',
+        'src/popup/popup.css',
+        '-o',
+        'bin/popup/popup.css',
+        '--minify',
+    ];
+
+    const tailwind = Bun.spawnSync(['bunx', 'tailwindcss', ...tailwindArgs]);
+    if (!tailwind.success) {
+        console.log('❌ Tailwind build failed');
+        console.log(tailwind.stderr);
+        return process.exit(1);
+    }
+
+    console.log('🪽 Tailwind built');
+
+    try {
+        await copyFile('src/manifest.json', 'bin/manifest.json');
+        await copyFile('src/popup/popup.html', 'bin/popup/popup.html');
+        console.log('📁Static files copied');
+    } catch (error) {
+        console.error('❌ Static files copy failed', error);
+        return process.exit(1);
+    }
+
+    console.log('\n✅ Extension build succeeded');
+})();
